b0ad568272d7f51236f92652b5465b830eca2f01
BrookeBryski <brookeabryski@gmail.com>
2022-02-01 11:38:52 -0600

    initial commit

---
+++ src/main/java/com/example/gsaccessingneo4jdatawithrest/GsAccessingNeo4jDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingneo4jdatawithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingNeo4jDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingNeo4jDataWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsaccessingneo4jdatawithrest/GsAccessingNeo4jDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingneo4jdatawithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingNeo4jDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
